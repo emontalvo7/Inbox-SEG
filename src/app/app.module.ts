fac9509e4b140e386011a63d49f31295486e3e1c
emontalvo7 <a3518110052@alumno.uttehuacan.edu.mx>
2023-08-09 17:04:00 -0600

    Version Inbox hardcode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { AccordionComponent } from './shared/accordion/accordion.component';
+import { TableComponent } from './shared/table/table.component';
+import { OffcanvasComponent } from './shared/offcanvas/offcanvas.component';
+import { PaginadorComponent } from './shared/paginador/paginador.component';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AccordionComponent,
+    TableComponent,
+    OffcanvasComponent,
+    PaginadorComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    TableModule,
     AppRoutingModule
   ],
   providers: [],
